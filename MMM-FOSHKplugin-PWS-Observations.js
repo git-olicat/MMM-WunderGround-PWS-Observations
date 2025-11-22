73efe8f15d630cb2ef744563302a657f926ee6fd
root <root@oli-ubuntu2.localnet>
2025-11-22 20:42:21 +0100

    WH51 1..16

--- MMM-FOSHKplugin-PWS-Observations.js
+++ MMM-FOSHKplugin-PWS-Observations.js
@@ -215,6 +215,16 @@ Module.register("MMM-FOSHKplugin-PWS-Observations", {
     var row28_sitrep = document.createElement("tr");
     var row29_sitrep = document.createElement("tr");
     var row30_sitrep = document.createElement("tr");
+    var row31_sitrep = document.createElement("tr");
+    var row32_sitrep = document.createElement("tr");
+    var row33_sitrep = document.createElement("tr");
+    var row34_sitrep = document.createElement("tr");
+    var row35_sitrep = document.createElement("tr");
+    var row36_sitrep = document.createElement("tr");
+    var row37_sitrep = document.createElement("tr");
+    var row38_sitrep = document.createElement("tr");
+    var row39_sitrep = document.createElement("tr");
+    var row40_sitrep = document.createElement("tr");
   
     if (this.config.wind == "1"){
       var windDirectionIcon = document.createElement("td");
@@ -594,6 +604,187 @@ Module.register("MMM-FOSHKplugin-PWS-Observations", {
       row29_sitrep.appendChild(temperature8);
       table_sitrep.appendChild(row29_sitrep);
     }
+
+    if (this.config.soilmoisture == "1"){ 
+      var soilmoistureIcon = document.createElement("td");
+      soilmoistureIcon.className = "pop wi wi-hot lpad";
+      row30_sitrep.appendChild(soilmoistureIcon);
+
+      var soilmoistureTxt = document.createElement("td");
+      soilmoistureTxt.className = "popr";
+      soilmoistureTxt.innerHTML = this.soilmoisture + "%";
+      row30_sitrep.appendChild(soilmoistureTxt);
+      table_sitrep.appendChild(row30_sitrep);
+    }
+    if (this.config.soilmoisture2 == "1"){ 
+      var soilmoisture2Icon = document.createElement("td");
+      soilmoisture2Icon.className = "pop wi wi-hot lpad";
+      row31_sitrep.appendChild(soilmoisture2Icon);
+
+      var soilmoisture2Txt = document.createElement("td");
+      soilmoisture2Txt.className = "popr";
+      soilmoisture2Txt.innerHTML = this.soilmoisture2 + "%";
+      row31_sitrep.appendChild(soilmoisture2Txt);
+      table_sitrep.appendChild(row31_sitrep);
+    }
+    if (this.config.soilmoisture3 == "1"){ 
+      var soilmoisture3Icon = document.createElement("td");
+      soilmoisture3Icon.className = "pop wi wi-hot lpad";
+      row32_sitrep.appendChild(soilmoisture3Icon);
+
+      var soilmoisture3Txt = document.createElement("td");
+      soilmoisture3Txt.className = "popr";
+      soilmoisture3Txt.innerHTML = this.soilmoisture3 + "%";
+      row33_sitrep.appendChild(soilmoisture3Txt);
+      table_sitrep.appendChild(row33_sitrep);
+    }
+    if (this.config.soilmoisture4 == "1"){ 
+      var soilmoisture4Icon = document.createElement("td");
+      soilmoisture4Icon.className = "pop wi wi-hot lpad";
+      row34_sitrep.appendChild(soilmoisture4Icon);
+
+      var soilmoisture4Txt = document.createElement("td");
+      soilmoisture4Txt.className = "popr";
+      soilmoisture4Txt.innerHTML = this.soilmoisture4 + "%";
+      row34_sitrep.appendChild(soilmoisture4Txt);
+      table_sitrep.appendChild(row34_sitrep);
+    }
+    if (this.config.soilmoisture5 == "1"){ 
+      var soilmoisture5Icon = document.createElement("td");
+      soilmoisture5Icon.className = "pop wi wi-hot lpad";
+      row35_sitrep.appendChild(soilmoisture5Icon);
+
+      var soilmoisture5Txt = document.createElement("td");
+      soilmoisture5Txt.className = "popr";
+      soilmoisture5Txt.innerHTML = this.soilmoisture5 + "%";
+      row35_sitrep.appendChild(soilmoisture5Txt);
+      table_sitrep.appendChild(row35_sitrep);
+    }
+    if (this.config.soilmoisture6 == "1"){ 
+      var soilmoisture6Icon = document.createElement("td");
+      soilmoisture6Icon.className = "pop wi wi-hot lpad";
+      row36_sitrep.appendChild(soilmoisture6Icon);
+
+      var soilmoisture6Txt = document.createElement("td");
+      soilmoisture6Txt.className = "popr";
+      soilmoisture6Txt.innerHTML = this.soilmoisture6 + "%";
+      row36_sitrep.appendChild(soilmoisture6Txt);
+      table_sitrep.appendChild(row36_sitrep);
+    }
+    if (this.config.soilmoisture7 == "1"){ 
+      var soilmoisture7Icon = document.createElement("td");
+      soilmoisture7Icon.className = "pop wi wi-hot lpad";
+      row37_sitrep.appendChild(soilmoisture7Icon);
+
+      var soilmoisture7Txt = document.createElement("td");
+      soilmoisture7Txt.className = "popr";
+      soilmoisture7Txt.innerHTML = this.soilmoisture7 + "%";
+      row37_sitrep.appendChild(soilmoisture7Txt);
+      table_sitrep.appendChild(row37_sitrep);
+    }
+    if (this.config.soilmoisture8 == "1"){ 
+      var soilmoisture8Icon = document.createElement("td");
+      soilmoisture8Icon.className = "pop wi wi-hot lpad";
+      row35_sitrep.appendChild(soilmoisture8Icon);
+
+      var soilmoisture8Txt = document.createElement("td");
+      soilmoisture8Txt.className = "popr";
+      soilmoisture8Txt.innerHTML = this.soilmoisture8 + "%";
+      row38_sitrep.appendChild(soilmoisture8Txt);
+      table_sitrep.appendChild(row38_sitrep);
+    }
+    if (this.config.soilmoisture9 == "1"){ 
+      var soilmoisture9Icon = document.createElement("td");
+      soilmoisture9Icon.className = "pop wi wi-hot lpad";
+      row39_sitrep.appendChild(soilmoisture9Icon);
+
+      var soilmoisture9Txt = document.createElement("td");
+      soilmoisture9Txt.className = "popr";
+      soilmoisture9Txt.innerHTML = this.soilmoisture9 + "%";
+      row39_sitrep.appendChild(soilmoisture9Txt);
+      table_sitrep.appendChild(row39_sitrep);
+    }
+    if (this.config.soilmoisture10 == "1"){ 
+      var soilmoisture10Icon = document.createElement("td");
+      soilmoisture10Icon.className = "pop wi wi-hot lpad";
+      row40_sitrep.appendChild(soilmoisture10Icon);
+
+      var soilmoisture10Txt = document.createElement("td");
+      soilmoisture10Txt.className = "popr";
+      soilmoisture10Txt.innerHTML = this.soilmoisture10 + "%";
+      row40_sitrep.appendChild(soilmoisture10Txt);
+      table_sitrep.appendChild(row40_sitrep);
+    }
+    if (this.config.soilmoisture11 == "1"){ 
+      var soilmoisture11Icon = document.createElement("td");
+      soilmoisture11Icon.className = "pop wi wi-hot lpad";
+      row41_sitrep.appendChild(soilmoisture11Icon);
+
+      var soilmoisture11Txt = document.createElement("td");
+      soilmoisture11Txt.className = "popr";
+      soilmoisture11Txt.innerHTML = this.soilmoisture11 + "%";
+      row41_sitrep.appendChild(soilmoisture11Txt);
+      table_sitrep.appendChild(row41_sitrep);
+    }
+    if (this.config.soilmoisture12 == "1"){ 
+      var soilmoisture12Icon = document.createElement("td");
+      soilmoisture12Icon.className = "pop wi wi-hot lpad";
+      row42_sitrep.appendChild(soilmoisture12Icon);
+
+      var soilmoisture12Txt = document.createElement("td");
+      soilmoisture12Txt.className = "popr";
+      soilmoisture12Txt.innerHTML = this.soilmoisture12 + "%";
+      row42_sitrep.appendChild(soilmoisture12Txt);
+      table_sitrep.appendChild(row42_sitrep);
+    }
+    if (this.config.soilmoisture13 == "1"){ 
+      var soilmoisture13Icon = document.createElement("td");
+      soilmoisture13Icon.className = "pop wi wi-hot lpad";
+      row43_sitrep.appendChild(soilmoisture13Icon);
+
+      var soilmoisture13Txt = document.createElement("td");
+      soilmoisture13Txt.className = "popr";
+      soilmoisture13Txt.innerHTML = this.soilmoisture13 + "%";
+      row43_sitrep.appendChild(soilmoisture13Txt);
+      table_sitrep.appendChild(row43_sitrep);
+    }
+    if (this.config.soilmoisture14 == "1"){ 
+      var soilmoisture14Icon = document.createElement("td");
+      soilmoisture14Icon.className = "pop wi wi-hot lpad";
+      row44_sitrep.appendChild(soilmoisture14Icon);
+
+      var soilmoisture14Txt = document.createElement("td");
+      soilmoisture14Txt.className = "popr";
+      soilmoisture14Txt.innerHTML = this.soilmoisture14 + "%";
+      row44_sitrep.appendChild(soilmoisture14Txt);
+      table_sitrep.appendChild(row44_sitrep);
+    }
+    if (this.config.soilmoisture15 == "1"){ 
+      var soilmoisture15Icon = document.createElement("td");
+      soilmoisture15Icon.className = "pop wi wi-hot lpad";
+      row45_sitrep.appendChild(soilmoisture15Icon);
+
+      var soilmoisture15Txt = document.createElement("td");
+      soilmoisture15Txt.className = "popr";
+      soilmoisture15Txt.innerHTML = this.soilmoisture15 + "%";
+      row45_sitrep.appendChild(soilmoisture15Txt);
+      table_sitrep.appendChild(row45_sitrep);
+    }
+    if (this.config.soilmoisture16 == "1"){ 
+      var soilmoisture16Icon = document.createElement("td");
+      soilmoisture16Icon.className = "pop wi wi-hot lpad";
+      row46_sitrep.appendChild(soilmoisture16Icon);
+
+      var soilmoisture16Txt = document.createElement("td");
+      soilmoisture16Txt.className = "popr";
+      soilmoisture16Txt.innerHTML = this.soilmoisture16 + "%";
+      row46_sitrep.appendChild(soilmoisture16Txt);
+      table_sitrep.appendChild(row46_sitrep);
+    }
+
+
+
+
 
 
 
